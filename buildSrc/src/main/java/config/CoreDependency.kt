1078fb6a21b6274de3927b6c0a758068c033eedf
Jieyi <pokkbaby@gmail.com>
2019-11-19 08:48:59 +0900

    build(build): bump to kotlin 1.3.60

--- buildSrc/src/main/java/config/CoreDependency.kt
+++ buildSrc/src/main/java/config/CoreDependency.kt
@@ -26,7 +26,7 @@ package config
 
 object CoreDependency {
     object Version {
-        const val KOTLIN = "1.3.60-eap-143"
+        const val KOTLIN = "1.3.60"
         const val KOTLIN_COROUTINE = "1.3.2"
     }
 
